9c60bfd3f104667a556f52772dfd09edab66879f
DevExpressExampleBot <fakeEmail>
2018-08-16 13:24:14 +0300

    Source auto update [skip ci]

---
+++ VB/Application.xaml.vb
@@ -0,0 +1,24 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Configuration
+Imports System.Data
+Imports System.Linq
+Imports System.Windows
+Imports System.Windows.Threading
+Imports DevExpress.Xpf.Core
+
+Namespace DXSample
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Partial Public Class App
+        Inherits Application
+
+        Public Sub New()
+
+        End Sub
+        Protected Overrides Sub OnStartup(ByVal e As StartupEventArgs)
+            MyBase.OnStartup(e)
+        End Sub
+    End Class
+End Namespace
