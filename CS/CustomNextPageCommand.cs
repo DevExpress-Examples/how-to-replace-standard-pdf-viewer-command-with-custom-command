55e842c064ce507bef4d404320a6d246df305bb5
Alexander Temnov <alexander.temnov@devexpress.com>
2018-08-16 12:50:06 +0300

    add example content

---
+++ CS/CustomNextPageCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DXSample {
+    public class CustomNextPageCommand : ICommand {
+        ICommand baseCommand;
+        public CustomNextPageCommand(ICommand baseCommand) {
+            this.baseCommand = baseCommand;
+        }
+
+        public event EventHandler CanExecuteChanged {
+            add { baseCommand.CanExecuteChanged += value; }
+            remove { baseCommand.CanExecuteChanged -= value; }
+        }
+
+        public bool CanExecute(object parameter) {
+            return baseCommand.CanExecute(parameter);
+
+        }
+
+        public void Execute(object parameter) {
+            baseCommand.Execute(parameter);
+        }
+    }
+}
---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using DevExpress.Xpf.PdfViewer;
+
+namespace DXSample {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+
+    public class CustomPdfCommandProvider : PdfCommandProvider {
+
+        protected override ICommand NextPageCommandInternal {
+            get {
+                return new CustomNextPageCommand(base.NextPageCommandInternal);
+            }
+        }
+
+        protected override ICommand PreviousPageCommandInternal {
+            get {
+                return new CustomPreviousPageCommand(base.PreviousPageCommandInternal);
+            }
+        }
+    }
+}
